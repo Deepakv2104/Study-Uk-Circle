bfbdd2d738bbbc9733a49039d6c9016668156ecb
Deepak Vishwakarma <90350741+Deepakv2104@users.noreply.github.com>
2024-03-13 21:17:33 +0530

    Update Navbar.js
    sme

--- src/components/Admin/Navbar.js
+++ src/components/Admin/Navbar.js
@@ -56,7 +56,7 @@ const AnimatedNav = () => {
             <li className="nav-item"><a >HOME</a></li>
             <li className="nav-item"><a >EVENTS</a></li>
             <li className="nav-item"><a >INTERNSHIPS</a></li>
-            <li className="nav-item"><a>GALLARY</a></li>
+            <li className="nav-item"><a>SME</a></li>
             <li className="nav-item" onClick={handleLogin}><a >LOGIN</a></li>
           </ul>
         </nav>
