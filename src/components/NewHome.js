dc3fea4aa49a066ef00378a9611099710c94cba9
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-04-29 20:33:49 +0530

    homepage minor changes

--- src/components/NewHome.js
+++ src/components/NewHome.js
@@ -135,7 +135,7 @@ const handleJoinClick =()=>{
         <div className="bento m" style={{ display: 'flex', justifyContent: 'space-between' }}>
           <div>
             <div className="limit-large">
-              <h2 className="heading">Are you studying in UK ? Looking for <span className="text-color-green">Mentorship, </span><span className="text-color-green">Accommodation</span> and <span className="text-color-green">  Jobs ?</span></h2>
+              <h2 className="heading">Are you studying in UK ? Looking for <span className="text-color-green">Mentorship, </span><span className="text-color-green">Accommodation</span> and <span className="text-color-green"><br/>Jobs ?</span></h2>
               {/* <div className="title-tag-wrapper">
                 <div className="title-tag-contents">
                   <div>United Kingdom</div>
@@ -331,7 +331,7 @@ const handleJoinClick =()=>{
                             
                         <p class="large-text white-text">WorldLynk is your one-stop solution for seamless student experience in UK.</p>
                 </div>
-                <div class="social-logos-wrap">
+                {/* <div class="social-logos-wrap">
                     <a href="/" target="_blank" class="social-logo w-inline-block">
                         <div class="icon-embed-xsmall w-embed"></div>
                     </a>
@@ -344,7 +344,8 @@ const handleJoinClick =()=>{
                     <a href="/" target="_blank" class="social-logo w-inline-block">
                         <div class="icon-embed-xsmall w-embed"></div>
                     </a>
-                </div>
+                </div> */}
+
             </div>
             <div id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d77-8da07d68" class="footer-columns">
                 <div     id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d78-8da07d68" class="footer-link-column">
@@ -352,24 +353,24 @@ const handleJoinClick =()=>{
                     <a href="/" id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d7b-8da07d68" aria-current="page" class="footer-link w-inline-block w--current">
                         <div>Home </div>
                     </a>
-                    <a href="/partnerships" id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d7e-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d7e-8da07d68" class="footer-link w-inline-block">
                         <div>Partnerships</div>
                     </a>
-                    <a href="/about" id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d94-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d94-8da07d68" class="footer-link w-inline-block">
                         <div>About</div>
                     </a>
-                    <a href="/newsroom" id="w-node-_0e8a5424-6d07-d04a-8956-a42956017799-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-_0e8a5424-6d07-d04a-8956-a42956017799-8da07d68" class="footer-link w-inline-block">
                         <div>Newsroom</div>
                     </a>
                     <div class="alignment-line smaller"></div>
-                    <a href="/contact" id="w-node-b07bc73a-1591-149d-a41a-d1699793fdbe-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-b07bc73a-1591-149d-a41a-d1699793fdbe-8da07d68" class="footer-link w-inline-block">
                         <div>Contact</div>
                     </a>
                 </div>
                 <div id="w-node-_3cf6e9e0-961c-5f0d-35c1-94757fe19ece-8da07d68" class="footer-stack">
                     <div id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d97-8da07d68" class="footer-link-column">
                         <div id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d98-8da07d68" class="footer-column-titile">Platform</div>
-                        <a id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d9a-8da07d68" class="footer-link w-inline-block">
+                        <a href="/join-waiting-list" id="w-node-f3b10b78-278d-2c6b-c975-d2648da07d9a-8da07d68" class="footer-link w-inline-block">
                             <div>Join </div>
                         </a>
                         <a href="/join-waiting-list" id="w-node-_05bed204-af49-6806-586c-d0440a0b7f82-8da07d68" class="footer-link w-inline-block">
@@ -377,35 +378,38 @@ const handleJoinClick =()=>{
                         </a>
                         <div class="alignment-line smaller"></div>
                     </div>
-                    <div class="footer-link-column">
+                    {/* <div class="footer-link-column">
                         <div id="w-node-d0e7d79e-52d4-f3ef-3f89-3bbd8596fcef-8da07d68" class="footer-column-titile">Service</div>
-                        <a href="/partnerships/youth-verified" id="w-node-d0e7d79e-52d4-f3ef-3f89-3bbd8596fcf1-8da07d68" class="footer-link w-inline-block">
+                        <a href="/" id="w-node-d0e7d79e-52d4-f3ef-3f89-3bbd8596fcf1-8da07d68" class="footer-link w-inline-block">
                             <div>Verification</div>
                         </a>
                         <div class="alignment-line smaller"></div>
-                    </div>
+                    </div> */}
                 </div>
                 <div id="w-node-_1b1e73d0-b89e-9f53-3549-ef6d4862bba4-8da07d68" class="footer-link-column">
                     <div id="w-node-_1b1e73d0-b89e-9f53-3549-ef6d4862bba5-8da07d68" class="footer-column-titile">Solution</div>
-                    <a href="/partnerships/talent" id="w-node-_1b1e73d0-b89e-9f53-3549-ef6d4862bba7-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-_1b1e73d0-b89e-9f53-3549-ef6d4862bba7-8da07d68" class="footer-link w-inline-block">
                         <div>Talent </div>
                     </a>
-                    <a href="/partnerships/communications" id="w-node-_2da2c486-3f70-1690-3115-0e283b12c042-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-_2da2c486-3f70-1690-3115-0e283b12c042-8da07d68" class="footer-link w-inline-block">
                         <div>Communications</div>
                     </a>
-                    <a href="/partnerships/development" id="w-node-_3c8ac7a1-bd89-3e61-ee45-f00bacee3c9b-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-_3c8ac7a1-bd89-3e61-ee45-f00bacee3c9b-8da07d68" class="footer-link w-inline-block">
                         <div>Development</div>
                     </a>
-                    <a href="/partnerships/pricing" id="w-node-fdf00af3-65a4-8598-ffe5-6cc32eb0133e-8da07d68" class="footer-link w-inline-block">
+                    <a href="/" id="w-node-fdf00af3-65a4-8598-ffe5-6cc32eb0133e-8da07d68" class="footer-link w-inline-block">
                         <div>Pricing</div>
                     </a>
                     <div class="alignment-line smaller"></div>
                 </div>
             </div>
         </div>
-       
+        <p class="large-text white-text">@copyrights reserved by Jashwanth</p>
+
         </div>
+
     </section>
+
     </div>
   )
 }
