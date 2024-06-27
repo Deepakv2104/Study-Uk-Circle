ff070bc4bec8b031f1d2dd1a98f9d56ddd617b09
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-06-27 23:50:36 +0530

    dkfd

--- src/components/NewNav.js
+++ src/components/NewNav.js
@@ -54,14 +54,7 @@ const NewNav = () => {
                             Sign-In
                         </div>
                     </li>
-                    <li className="nav-item">
-                        <div
-                            onClick={() => { navigate('/privacy-policy'); closeMobileMenu(); }}
-                            className="nav-link"
-                        >
-                           Terms & Conditions
-                        </div>
-                    </li>
+                   
                 </ul>
             </div>
         </nav>
