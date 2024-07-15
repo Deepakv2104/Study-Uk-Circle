80528b79c0e5133a2f7cded09b13c96d32940131
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-07-16 00:35:16 +0530

    events imgs transfered from firestore to client side for less reads

--- src/components/Student/Events/CardSlider.js
+++ src/components/Student/Events/CardSlider.js
@@ -8,18 +8,29 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './CardSlider.css';
 
-const categories = [
-  { name: "Academic Events", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Facademic%20events.jpg?alt=media&token=6ac2e42d-daac-4470-a1b0-c0db106ea740" },
-  { name: "Cultural Events", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Fcultural%20events.jpg?alt=media&token=25640cc9-35ac-497f-a115-954ca7cccdb9" },
-  { name: "Social Events", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Fsocial%20events.jpg?alt=media&token=85df17bb-62c1-43b5-bca3-11c0205849e4" },
-  { name: "Sports and Recreation", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Fsports%20events.jpg?alt=media&token=95a6345e-400d-4c8a-bea6-f95d8e85570a" },
-  { name: "Professional Development", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Fprofessional%20development.jpg?alt=media&token=7c741c38-9383-48d2-b375-028145949f81" },
-  { name: "Health and Wellness", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Fhealth%20and%20wellness.jpg?alt=media&token=0ddf4b74-35d9-4e34-a7c2-8171f89f5fb1" },
-  { name: "Orientation and Information Sessions", imageUrl: "https://cdn-ember.fatsoma.com/assets/models/images/category/category-comedy-fc098385c6204c4abf2ced0b1d397725.jpg" },
-  { name: "Arts and Entertainment", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Fart.jpg?alt=media&token=42108d75-b8bf-4856-b592-23f51c5e1b24" },
-  { name: "Technology and Innovation", imageUrl: "https://firebasestorage.googleapis.com/v0/b/worldlynk-97994.appspot.com/o/assets%2Ftech.jpg?alt=media&token=e5a71b37-b49b-4d97-9026-8f79a9e2c0e4" }
-];
+import academicEvents from './img/academic-events.jpg'
+import art from './img/art.jpg'
+import cultural from './img/cultural-events.jpg'
+import eventsCover from './img/events_cover.jpg'
+import healthAndWellness from './img/health-and-wellness.jpg'
+import professionalDev from './img/professional-development.jpg'
+import social from './img/social-events.jpg'
+import sports from './img/sports-events.jpg'
+import tech from './img/tech.jpg'
+import orientation from './img/orientation.png'
 
+  const categories = [
+    { name: "Academic Events", imageUrl: academicEvents },
+    { name: "Cultural Events", imageUrl: cultural },
+    { name: "Social Events", imageUrl: social },
+    { name: "Sports and Recreation", imageUrl: sports },
+    { name: "Professional Development", imageUrl: professionalDev },
+    { name: "Health and Wellness", imageUrl: healthAndWellness },
+    { name: "Orientation and Information Sessions", imageUrl: orientation },
+    { name: "Arts and Entertainment", imageUrl: art },
+    { name: "Technology and Innovation", imageUrl: tech }
+  ];
+  
 const CardSlider = () => {
   const navigate = useNavigate();
 
