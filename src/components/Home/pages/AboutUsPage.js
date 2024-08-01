47842529d2e8578309f99e25484897da8222ea8c
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-08-01 17:10:08 +0530

    added placeholder before video

--- src/components/Home/pages/AboutUsPage.js
+++ src/components/Home/pages/AboutUsPage.js
@@ -1,76 +1,73 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from 'react';
 import NewNav from '../sub-components/NewNav';
 import Footer from '../sub-components/Footer';
 import { FaBullseye, FaLightbulb, FaUsers } from 'react-icons/fa';
 import './AboutUsPage.css';
-import globe from '../../../assets/vid/globe.mp4'
+import globe from '../../../assets/vid/globe.mp4';
+import placeholderImage from '../../../assets/img/globe-placeholder.png'; // Import your placeholder image
+
 const AboutUsPage = () => {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  useEffect(() => {
+    const video = document.querySelector('video');
+    video.addEventListener('loadeddata', () => setIsVideoLoaded(true));
+    return () => video.removeEventListener('loadeddata', () => setIsVideoLoaded(true));
+  }, []);
+
   return (
     <div className="text-white min-h-screen">
       <NewNav />
       <div className="relative overflow-hidden">
-        <motion.div
-          className="bg-video-wrapper"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 2 }}
-        >
-          <video className="bg-video" autoPlay loop muted>
+        <div className="bg-video-wrapper">
+          {!isVideoLoaded && (
+            <img 
+              src={placeholderImage} 
+              alt="Placeholder" 
+              className="bg-image"
+            />
+          )}
+          <video 
+            className={`bg-video ${isVideoLoaded ? 'visible' : 'hidden'}`} 
+            autoPlay 
+            loop 
+            muted
+          >
             <source src={globe} type="video/mp4" />
           </video>
-        </motion.div>
+        </div>
         <div className="container mx-auto py-16 px-4 lg:px-8 relative">
-          <motion.div
-            className="text-center mb-16"
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-          >
+          <div className="text-center mb-16">
             <h1 className="text-5xl font-bold mb-4">About Us</h1>
             <p className="text-lg text-gray-200">Get to know more about our mission, vision, and values</p>
-          </motion.div>
+          </div>
           <div className="grid gap-12 md:grid-cols-3">
-            <motion.div
-              className="flex flex-col items-center text-center"
-              whileHover={{ scale: 1.1, rotate: 5 }}
-            >
+            <div className="flex flex-col items-center text-center">
               <FaBullseye className="text-5xl text-blue-500 mb-6" />
               <h3 className="text-2xl font-semibold mb-2">Personalized Experiences</h3>
               <p className="text-gray-200">
                 <strong>Goal:</strong> Tailor the platform to meet users' needs and preferences. <br />
                 <strong>Achievement:</strong> Provide relevant recommendations and a customized user experience.
               </p>
-            </motion.div>
-            <motion.div
-              className="flex flex-col items-center text-center"
-              whileHover={{ scale: 1.1, rotate: -5 }}
-            >
+            </div>
+            <div className="flex flex-col items-center text-center">
               <FaLightbulb className="text-5xl text-yellow-500 mb-6" />
               <h3 className="text-2xl font-semibold mb-2">Innovation & Improvement</h3>
               <p className="text-gray-200">
                 <strong>Goal:</strong> Evolve the platform with cutting-edge technologies. <br />
                 <strong>Achievement:</strong> Keep the platform up-to-date and relevant.
               </p>
-            </motion.div>
-            <motion.div
-              className="flex flex-col items-center text-center"
-              whileHover={{ scale: 1.1, rotate: 5 }}
-            >
+            </div>
+            <div className="flex flex-col items-center text-center">
               <FaUsers className="text-5xl text-green-500 mb-6" />
               <h3 className="text-2xl font-semibold mb-2">Supportive Community</h3>
               <p className="text-gray-200">
                 <strong>Goal:</strong> Cultivate a supportive community for users. <br />
                 <strong>Achievement:</strong> Create a sense of belonging and provide valuable networks.
               </p>
-            </motion.div>
+            </div>
           </div>
-          <motion.div
-            className="mt-16 text-center"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-          >
+          <div className="mt-16 text-center">
             <h2 className="text-3xl font-bold mb-4">Our Mission & Vision</h2>
             <p className="text-lg text-gray-200 mb-6">
               At WorldLynk, our mission is to empower individuals and communities by integrating leading services into a seamless experience. We enhance connectivity, accessibility, and productivity, making global information and opportunities universally accessible.
@@ -78,7 +75,7 @@ const AboutUsPage = () => {
             <p className="text-lg text-gray-200">
               We envision a world where digital connections transcend boundaries, enabling everyone to thrive. Our platform supports, inspires, and empowers future generations, creating a world of unlimited digital bridges.
             </p>
-          </motion.div>
+          </div>
         </div>
       </div>
       <Footer />
@@ -87,3 +84,5 @@ const AboutUsPage = () => {
 };
 
 export default AboutUsPage;
+
+
