83ebdb4a9fca205ec672a76e396da0fa564d9756
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-08-01 16:07:24 +0530

    aboutpage changed

--- src/components/Home/pages/AboutUsPage.js
+++ src/components/Home/pages/AboutUsPage.js
@@ -1,65 +1,84 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import NewNav from '../sub-components/NewNav';
 import Footer from '../sub-components/Footer';
 import { FaBullseye, FaLightbulb, FaUsers } from 'react-icons/fa';
-// import './NewHome1.css';
-
+import './AboutUsPage.css';
+import globe from '../../../assets/vid/globe.mp4'
 const AboutUsPage = () => {
   return (
-    <div>
+    <div className="text-white min-h-screen">
       <NewNav />
-      <div className="bg-gray-800 text-white min-h-screen">
-        <div className="join-container mx-auto max-w-5xl px-4 py-2">
-          <div className="left-column space-y-12">
-            <div className="heading mb-8 text-center">
-              <h3 className="text-4xl font-bold">About Us</h3>
-            </div>
-            <div className="space-y-8">
-              <div className="flex flex-col items-center text-center sm:flex-row sm:text-left sm:items-start sm:space-x-4">
-                <FaBullseye className="text-4xl text-blue-400 mb-4 sm:mb-0" />
-                <div>
-                  <h4 className="text-2xl font-semibold mb-4">Deliver Personalized Experiences</h4>
-                  <p className="large-text">
-                    <strong>Goal:</strong> Tailor the platform to meet users' needs and preferences.<br />
-                    <strong>Achievement:</strong> Provide relevant recommendations and a customized user experience that increases satisfaction and engagement.
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-col items-center text-center sm:flex-row sm:text-left sm:items-start sm:space-x-4">
-                <FaLightbulb className="text-4xl text-yellow-400 mb-4 sm:mb-0" />
-                <div>
-                  <h4 className="text-2xl font-semibold mb-4">Foster Innovation and Continuous Improvement</h4>
-                  <p className="large-text">
-                    <strong>Goal:</strong> Continuously evolve the platform by integrating cutting-edge technologies and responding to user feedback.<br />
-                    <strong>Achievement:</strong> Keep the platform up-to-date with the latest advancements and remain relevant and practical.
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-col items-center text-center sm:flex-row sm:text-left sm:items-start sm:space-x-4">
-                <FaUsers className="text-4xl text-green-400 mb-4 sm:mb-0" />
-                <div>
-                  <h4 className="text-2xl font-semibold mb-4">Build a Supportive Community</h4>
-                  <p className="large-text">
-                    <strong>Goal:</strong> Cultivate a community where users can share resources, seek advice, and collaborate on projects.<br />
-                    <strong>Achievement:</strong> Create a sense of belonging and provide valuable support networks for users.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="space-y-4">
-              <h4 className="text-2xl font-semibold mb-4">Our Company's Main Mission</h4>
-              <p className="large-text">
-                At WorldLynk, our mission is to empower individuals and communities by integrating the world's leading services into a seamless and personalized experience. We aim to enhance connectivity, accessibility, and productivity, ensuring that everyone can organize, access, and utilize information effortlessly. By linking professionals, students, and consumers globally, we strive to make learning, earning, and living more dynamic and inclusive, fostering a future where digital bridges are unlimited.
+      <div className="relative overflow-hidden">
+        <motion.div
+          className="bg-video-wrapper"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          <video className="bg-video" autoPlay loop muted>
+            <source src={globe} type="video/mp4" />
+          </video>
+        </motion.div>
+        <div className="container mx-auto py-16 px-4 lg:px-8 relative z-10">
+          <motion.div
+            className="text-center mb-16"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <h1 className="text-5xl font-bold mb-4">About Us</h1>
+            <p className="text-lg text-gray-300">Get to know more about our mission, vision, and values</p>
+          </motion.div>
+          <div className="grid gap-12 md:grid-cols-3">
+            <motion.div
+              className="flex flex-col items-center text-center"
+              whileHover={{ scale: 1.1, rotate: 5 }}
+            >
+              <FaBullseye className="text-5xl text-blue-500 mb-6" />
+              <h3 className="text-2xl font-semibold mb-2">Personalized Experiences</h3>
+              <p className="text-gray-400">
+                <strong>Goal:</strong> Tailor the platform to meet users' needs and preferences. <br />
+                <strong>Achievement:</strong> Provide relevant recommendations and a customized user experience.
               </p>
-              <p className="large-text">
-                WorldLynk envisions a world where digital connections transcend geographical boundaries, enabling individuals and communities to thrive. Our vision is to create an enduring platform that supports, inspires, and empowers the next generations, making global information and opportunities universally accessible and beneficial for all.
+            </motion.div>
+            <motion.div
+              className="flex flex-col items-center text-center"
+              whileHover={{ scale: 1.1, rotate: -5 }}
+            >
+              <FaLightbulb className="text-5xl text-yellow-500 mb-6" />
+              <h3 className="text-2xl font-semibold mb-2">Innovation & Improvement</h3>
+              <p className="text-gray-400">
+                <strong>Goal:</strong> Evolve the platform with cutting-edge technologies. <br />
+                <strong>Achievement:</strong> Keep the platform up-to-date and relevant.
               </p>
-            </div>
-            {/* <img src="https://join.getwyld.in/assets/images/line.png" alt="Divider" className="mb-8 mx-auto" /> */}
-            {/* <div className="text-center">
-            <a href="/" className="glass-button smaller w-button  rounded-lg bg-orange-500 hover:bg-orange-600" style={{ textDecoration: 'none', color: 'white', padding:'0.8rem' }}>Back to homepage</a>
-            </div> */}
+            </motion.div>
+            <motion.div
+              className="flex flex-col items-center text-center"
+              whileHover={{ scale: 1.1, rotate: 5 }}
+            >
+              <FaUsers className="text-5xl text-green-500 mb-6" />
+              <h3 className="text-2xl font-semibold mb-2">Supportive Community</h3>
+              <p className="text-gray-400">
+                <strong>Goal:</strong> Cultivate a supportive community for users. <br />
+                <strong>Achievement:</strong> Create a sense of belonging and provide valuable networks.
+              </p>
+            </motion.div>
           </div>
+          <motion.div
+            className="mt-16 text-center"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <h2 className="text-3xl font-bold mb-4">Our Mission & Vision</h2>
+            <p className="text-lg text-gray-400 mb-6">
+              At WorldLynk, our mission is to empower individuals and communities by integrating leading services into a seamless experience. We enhance connectivity, accessibility, and productivity, making global information and opportunities universally accessible.
+            </p>
+            <p className="text-lg text-gray-400">
+              We envision a world where digital connections transcend boundaries, enabling everyone to thrive. Our platform supports, inspires, and empowers future generations, creating a world of unlimited digital bridges.
+            </p>
+          </motion.div>
         </div>
       </div>
       <Footer />
